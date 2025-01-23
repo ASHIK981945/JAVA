c9bf5b3cfd531aeb25cd8d605d1de6e7d8fc6938
ASHIK981945 <163637890+ASHIK981945@users.noreply.github.com>
2025-01-23 17:46:17 +0545

    java

---
+++ Datatypes.java
@@ -0,0 +1,50 @@
+public class Datatypes {
+    public static void main (String[] args){
+        // variables --> variables are like containers  where  values can store .
+        // datatypes --> the types of data that variable can hold.
+         // case-senssitive (a not equal )
+        // java is a ...statically typed language ...  which means every variable must be have specific data tpyes before use
+
+        // types of  datatypes
+
+        //  primitive data types it specifies the type and kind of value it can hold .
+        // the different types of  primitive datatypes  are given as below
+
+        //  1) 1 byte : (size: 1 byte ),(range -127 to 128)
+        // byte byteExample =100;
+        // System.out.println(byteExample);
+
+        // 2) short:( size 2 bytes ), ( range -32,768 to 32,767)
+        // short shortExample = 32767;
+        // System.out.println(shortExample);
+
+        // 3) int:(size 4 bytes ), (range -2^31 to 2^31-1)
+        // int intExample = 1000000;
+        // System.out.println(intExample);
+
+      
+        // 4) long:(size 8 bytes )
+        // long longexample= 1234567890L;
+        // System.out.println(longexample);
+
+         // 5) float:( store values for decimal points , sufficient for storing 6 to 7 decimal)
+        // float floatExample = 123.456f;
+        // System.out.println(floatExample);
+
+        // 6) double :( store values for decimal points, sufficient for storing 15 to 16 decimal)
+        // double doubleexample=11.123d;
+        // System.out.println(doubleexample);
+
+        // 7) boolean:(store boolean values either true or false)
+        // boolean booleanExample = true;
+        // System.out.println(booleanExample);
+        
+        //8) char :(store a single character or ASCII values { 65-A 97-a ASCII CODE})
+        // char charExample = 'A';
+        // System.out.println(charExample);
+
+
+    }
+}
+
+
---
+++ Particles.java
@@ -0,0 +1,16 @@
+
+//  public class Particles{
+//     public static void main(String[] args) {
+//         System.out.println("hello world");
+//     }
+
+//  }
+
+ public class Particles{
+    public static void main (String [] args) {
+        String name = "ashik" ;
+        System.out.println(name);
+    }
+ }
+
+ 
