4d09928f9e04b1e4eb14bbb0f2bd772f08f15fe2
ASHIK981945 <163637890+ASHIK981945@users.noreply.github.com>
2025-01-27 21:14:06 +0545

    java

---
+++ Assignment.java
@@ -0,0 +1,26 @@
+public class Assignment {
+    public static void main(String[] args) {
+
+        // add two integer number
+        // byte a = 12;
+        // byte b = 34;
+        // int add = (a + b);
+        // System.out.println("Addition of two numbers is " + add);
+
+        // subtract two float number
+        // byte c = 12;
+        // byte d = 34;
+        // float sub = (c - d);
+        // System.out.println("Subtraction of two numbers is " + sub);
+
+        // print the world (java is a programming language)
+        // String world = "java is a programming language";
+        // System.out.println(world);
+
+        // what is the ASCII value FOR 66?
+        // The ASCII value 66 corresponds to the letter "B".
+        // Explanation: In the ASCII table, the uppercase letter "B" has a decimal value
+        // of 66
+
+    }
+}
