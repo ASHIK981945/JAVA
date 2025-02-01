688310389d40496613149c5e7d1b36b8415cd630
ASHIK981945 <163637890+ASHIK981945@users.noreply.github.com>
2025-02-01 21:26:36 +0545

    java

--- ASSIGNMENT/Bitwiseoperator.java
+++
@@ -1,8 +0,0 @@
-package ASSIGNMENT;
-
-public class Bitwiseoperation {
-    public static void main(String[] args) {
-        int a = 5;
-        System.out.println("~ of a " + a);
-    }
-}
--- Particles.java
+++ Particles.java
@@ -16,6 +16,16 @@ public class Particles {
         int addResult = a + b;
         int subResult = a / b;
         System.out.println("Addition result is " + addResult);
+        System.out.println("----------------------");
         System.out.println("Subtract result is " + subResult);
+        System.out.println("----------------------");
+
+        int x = 30;
+        if (x < 20) {
+            System.out.print("This is if statement");
+        } else {
+            System.out.print("This is else statement");
+            System.out.println("----------------------");
+        }
     }
 }
