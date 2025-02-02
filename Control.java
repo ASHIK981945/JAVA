2e8c50637bc60288cadc3aba9dd9e0bee9824e8a
ASHIK981945 <163637890+ASHIK981945@users.noreply.github.com>
2025-02-02 20:47:41 +0545

    java

---
+++ Control.java
@@ -0,0 +1,104 @@
+public class Control {
+    public static void main(String[] args) {
+        // control statements : conditional statement , looping statement
+
+        // control statements:
+        // if , if-else , if-elseif-else , switch
+
+            // if-else
+            // if (condition ){
+            // if block
+            //  }
+            // else {
+                // else block
+            // }
+
+            // int a = 10;
+            // if (age>=18){
+            //     System.out.println("you are eligible for voting");
+            // }
+            // else{
+            //     System.out.println("you are not eligible for voting");
+            //     }
+
+            // string message = (age >=18) ? "Eligible " : "not eligible";
+            
+
+            // int num =10 
+            // if (num>0){
+            //     System.out.println("postive number");
+            //     System.out.println("----------------------");
+            // }
+            // else if (num<0){
+            //     System.out.println("negative number");
+            //     System.out.println("----------------------");
+            // }
+            // else if (num ==0){
+            //     System.out.println("zero");
+            //     System.out.println("----------------------");
+            //     }
+            // else{
+            //     System.out.println("invalid number");
+            //     System.out.println("----------------------");
+            // }
+
+
+            // switch 
+
+            int day = 4;
+            switch (day){
+                case 1:
+                System.out.println("sunday");
+                break;
+                case 2:
+                System.out.println("monday");
+                break;
+                case 3:
+                System.out.println("tuesday");
+                break;
+                case 4:
+                System.out.println("wednesday");
+                break;
+                case 5:
+                System.out.println("thursday");
+                break;
+                case 6:
+                System.out.println("friday");
+                break;
+                case 7:
+                System.out.println("saturday");
+                break;
+                default:
+                System.out.println("invalid day");
+                break;
+                }
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //looping statements:
+        // while ,do-while ,for 
+
+
+
+    }
+}
