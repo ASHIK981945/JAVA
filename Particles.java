40fc0706d250da7f3dee3d8f7cbb743111508dc9
ASHIK981945 <163637890+ASHIK981945@users.noreply.github.com>
2025-02-06 19:17:26 +0545

    java

--- Particles.java
+++ Particles.java
@@ -11,21 +11,21 @@ public class Particles {
         // String name = "ashik" ;
         // System.out.println(name);
 
-        int a = 30;
-        int b = 20;
-        int addResult = a + b;
-        int subResult = a / b;
-        System.out.println("Addition result is " + addResult);
-        System.out.println("----------------------");
-        System.out.println("Subtract result is " + subResult);
-        System.out.println("----------------------");
+        // int a = 30;
+        // int b = 20;
+        // int addResult = a + b;
+        // int subResult = a / b;
+        // System.out.println("Addition result is " + addResult);
+        // System.out.println("----------------------");
+        // System.out.println("Subtract result is " + subResult);
+        // System.out.println("----------------------");
 
-        int x = 30;
-        if (x < 20) {
-            System.out.print("This is if statement");
-        } else {
-            System.out.print("This is else statement");
-            System.out.println("----------------------");
-        }
+        // int x = 30;
+        // if (x < 20) {
+        // System.out.print("This is if statement");
+        // } else {
+        // System.out.print("This is else statement");
+        // System.out.println("----------------------");
+        // }
     }
 }
