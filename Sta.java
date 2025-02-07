7a9f9402c334cc56f9e010183e91827efb23967a
ASHIK981945 <163637890+ASHIK981945@users.noreply.github.com>
2025-02-07 20:37:24 +0545

    java

--- Sta.java
+++ Sta.java
@@ -1,7 +1,8 @@
-public class  Sta {
-    public static void main (String[] agres){
-        int num = (1);
-        System.out.println( num);
-        System.out.println("ashik");
+public class Sta {
+    public static void main(String[] agres) {
+
+        // int num = (1);
+        // System.out.println( num);
+        // System.out.println("ashik");
     }
 }
