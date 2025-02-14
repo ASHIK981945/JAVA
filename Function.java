4cd87ae56fa7a8fe5a80f4d947965c8bfee2b11a
ASHIK981945 <163637890+ASHIK981945@users.noreply.github.com>
2025-02-14 08:13:52 +0545

    java

---
+++ Function.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Function {
+
+    // static int addition(int a, int b) {
+    // return a + b;
+    // }
+
+    static int multinum(int a, int b) {
+        return a * b;
+    }
+
+    public static void main(String[] args) {
+
+        // function
+        // int addition(){
+        // }
+
+        // int firstnumber = addition(10, 20);
+        // int secondnumber = addition(40, 50);
+        // System.out.println(firstnumber);
+        // System.out.println(secondnumber);
+
+        Scanner m = new Scanner(System.in);
+        System.out.println("enter the number");
+        int a = m.nextInt();
+        int b = m.nextInt();
+        int mutli = multinum(a, b);
+        System.out.println(mutli);
+
+        // int a = 10;
+        // int b = 20;
+        // int c = 30;
+        // int d = 50;
+        // int result = a + b + c + d ;
+        // System.out.println(result);
+
+    }
+}
