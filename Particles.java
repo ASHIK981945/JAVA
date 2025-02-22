12cbad95f2b83c44885a523f82c2cf710bded88e
ASHIK981945 <163637890+ASHIK981945@users.noreply.github.com>
2025-02-22 22:34:33 +0545

    java

--- Particles.java
+++ Particles.java
@@ -1,32 +1,32 @@
 
-//  public class Particles{
-//     public static void main(String[] args) {
-//         System.out.println("hello world");
-//     }
+// // public class Particles{
+// // public static void main(String[] args) {
+// // System.out.println("hello world");
+// // }
 
-//  }
+// // }
 
-public class Particles {
-    public static void main(String[] args) {
-        String name = "ashik";
-        System.out.println(name);
+// public class Particles {
+// public static void main(String[] args) {
+// String name = "ashik";
+// System.out.println(name);
 
-        int a = 30;
-        int b = 20;
-        int addResult = a + b;
-        int subResult = a / b;
-        System.out.println("Addition result is " + addResult);
-        System.out.println("----------------------");
-        System.out.println("Subtract result is " + subResult);
-        System.out.println("----------------------");
+// int a = 30;
+// int b = 20;
+// int addResult = a + b;
+// int subResult = a / b;
+// System.out.println("Addition result is " + addResult);
+// System.out.println("----------------------");
+// System.out.println("Subtract result is " + subResult);
+// System.out.println("----------------------");
 
-        int x = 30;
-        if (x < 20) {
-            System.out.print("This is if statement");
-        } else {
-            System.out.print("This is else statement");
-            System.out.println("----------------------");
-        }
+// int x = 30;
+// if (x < 20) {
+// System.out.print("This is if statement");
+// } else {
+// System.out.print("This is else statement");
+// System.out.println("----------------------");
+// }
 
-    }
-}
+// }
+// }
