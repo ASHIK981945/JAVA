b7d33d6866da8e95f519d58bcdd9c9d8434802aa
ASHIK981945 <163637890+ASHIK981945@users.noreply.github.com>
2025-05-31 14:42:21 +0545

    first comment

--- MDI/Mdiexample.java
+++ MDI/Mdiexample.java
@@ -1,55 +1,55 @@
 
-import javax.swing.JDesktopPane;
-import javax.swing.JFrame;
-import javax.swing.JInternalFrame;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import javax.swing.SwingUtilities;
+// import javax.swing.JDesktopPane;
+// import javax.swing.JFrame;
+// import javax.swing.JInternalFrame;
+// import javax.swing.JMenu;
+// import javax.swing.JMenuBar;
+// import javax.swing.JMenuItem;
+// import javax.swing.SwingUtilities;
 
-public class Mdiexample extends JFrame{
-    private final JDesktopPane desktopPanel;
-    public Mdiexample(){
-        setTitle("MDI Example using Jdesktop and Internal frame ");
-        setSize(800,600);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+// public class Mdiexample extends JFrame{
+//     private final JDesktopPane desktopPanel;
+//     public Mdiexample(){
+//         setTitle("MDI Example using Jdesktop and Internal frame ");
+//         setSize(800,600);
+//         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-        desktopPanel = new JDesktopPane();
-        add(desktopPanel);
+//         desktopPanel = new JDesktopPane();
+//         add(desktopPanel);
 
-        JMenuBar menuBar = new JMenuBar();
-        setJMenuBar(menuBar);
-        JMenu menu = new JMenu("File");
-        menuBar.add(menu);
+//         JMenuBar menuBar = new JMenuBar();
+//         setJMenuBar(menuBar);
+//         JMenu menu = new JMenu("File");
+//         menuBar.add(menu);
 
 
-        JMenuItem newItem = new JMenuItem("new window ");
-        newItem.addActionListener(e -> createInternalFrame());
-        menu.add(newItem);
+//         JMenuItem newItem = new JMenuItem("new window ");
+//         newItem.addActionListener(e -> createInternalFrame());
+//         menu.add(newItem);
 
-        JMenuItem exitItem = new JMenuItem("Exit");
-        exitItem.addActionListener(e -> System.exit(0));
-        menu.add(exitItem);
-    }
+//         JMenuItem exitItem = new JMenuItem("Exit");
+//         exitItem.addActionListener(e -> System.exit(0));
+//         menu.add(exitItem);
+//     }
 
-    private void createInternalFrame(){
-        JInternalFrame internalFrame = new JInternalFrame(
-            "Internal frame",true,true,true,true
-        );
-        internalFrame.setSize(300, 200);
-        internalFrame.setVisible(true);
+//     private void createInternalFrame(){
+//         JInternalFrame internalFrame = new JInternalFrame(
+//             "Internal frame",true,true,true,true
+//         );
+//         internalFrame.setSize(300, 200);
+//         internalFrame.setVisible(true);
 
-        desktopPanel.add(internalFrame);
+//         desktopPanel.add(internalFrame);
 
-        try {
-            internalFrame.setSelected(true);
-        } catch (java.beans.PropertyVetoException e) {
-            e.printStackTrace();
-        }
-    }
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            new Mdiexample().setVisible(true);
-        });
-    }
-}
+//         try {
+//             internalFrame.setSelected(true);
+//         } catch (java.beans.PropertyVetoException e) {
+//             e.printStackTrace();
+//         }
+//     }
+//     public static void main(String[] args) {
+//         SwingUtilities.invokeLater(() -> {
+//             new Mdiexample().setVisible(true);
+//         });
+//     }
+// }
